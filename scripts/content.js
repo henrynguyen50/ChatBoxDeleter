7dc45055bc85eebfe5490aa0b8d05d8c04814eb8
henrynguyen50 <henryhuynguyen17@gmail.com>
2025-04-26 16:45:06 -0500

    Streamedsu test success, have to put . before each class

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,7 @@
 //script to delete chat box
 
 //just for streamed.su first test pogchamp
-const element = document.querySelectorAll(".w-full h-auto border-border/40 border-2 rounded-lg drop-shadow-lg p-2 relative")
+const element = document.querySelector(".w-full.h-auto.border-border\\/40.border-2.rounded-lg.drop-shadow-lg.p-2.relative") //each class needs a . for it to be treated like css selector
 if (element) {
-    element.forEach(el => el.remove());
+    element.remove();
 }
