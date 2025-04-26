b4d485e056e94b81c0cc3614cc512b50de45d80e
henrynguyen50 <henryhuynguyen17@gmail.com>
2025-04-26 16:28:42 -0500

    Test for streamed.su

---
+++ content.js
@@ -0,0 +1,7 @@
+//script to delete chat box
+
+//just for streamed.su first test pogchamp
+const element = document.querySelectorAll(".w-full h-auto border-border/40 border-2 rounded-lg drop-shadow-lg p-2 relative")
+if (element) {
+    element.forEach(el => el.remove());
+}
