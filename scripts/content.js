eda5d61342adb4c2b6cbb159f02c3e29e0d38ee8
henrynguyen50 <henryhuynguyen17@gmail.com>
2025-05-16 18:27:43 -0500

    Added streameast support and added links to stream sites

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,8 @@
 //script to delete chat box
-
+//classes
 const chatBoxClasses = [
     ".w-full.h-auto.border-border\\/40.border-2.rounded-lg.drop-shadow-lg.p-2.relative", //streamedsu
+    ".style-scope.yt-live-chat-renderer", //STREAMEast app
     ".col-xl-3.chat-width.mb-2.mb-lg-0.pe-0", //ppvwtf
     ".stream-chat", //general ones that catch twitch and yt
     ".yt-live-chat-renderer",
@@ -11,9 +12,10 @@ const chatBoxClasses = [
     ".bg-gray-900.p-2.h-96.verflow-y-scroll.rounded"//crackstreams
 
 ]
-
+//divs
 const chatBoxIDs = [
-    "chat-col"
+    "chat-col",
+    "chat-messages"
 ]
 
 
