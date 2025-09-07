a069ec09c3f0e2e27caf35b5fac8ac8eec1c5890
henrynguyen50 <henrynguyen17@gmail.com>
2025-09-07 13:14:37 -0500

    Fixed ppv/ppvs

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,15 @@
 //script to delete chat box
 //classes
 const chatBoxClasses = [
+    
     ".w-full.h-auto.border-border\\/40.border-2.rounded-lg.drop-shadow-lg.p-2.relative", //streamedsu
     ".style-scope.yt-live-chat-renderer", //STREAMEast app
-    ".col-xl-3.chat-width.mb-2.mb-lg-0.pe-0", //ppvwtf
     ".stream-chat", //general ones that catch twitch and yt
     ".yt-live-chat-renderer",
     ".no-pm", //720pstream
     ".message-window",
     ".box", //daddylive
-    ".bg-gray-900.p-2.h-96.verflow-y-scroll.rounded",//crackstreams
+    //".bg-gray-900.p-2.h-96.verflow-y-scroll.rounded",//crackstreams
     ".live-chat-icon-container",
     //any chat that loads in after use iframe to remove
     'iframe[src="https://chat.dailywrestling.cc:4343/?aew"]', 
@@ -17,14 +17,14 @@ const chatBoxClasses = [
     'iframe[src^="https://chat.dailywrestling.cc:4343/"]', //catch all 
     'iframe[src="https://www2.cbox.ws/box/?boxid=2400909&boxtag=30q0rb&tid=1&tkey=5b7cdd27db70d81e"]',
     'iframe[src="https://st.chatango.com/h5/gz/r0610250550/id.html"]',
-    'iframe[src="https://www.youtube.com/live_chat?v=1-K8pOUDDO4&embed_domain=the.streameast.app&dark_theme=1"]'
+    'iframe[src="https://www.youtube.com/live_chat?v=1-K8pOUDDO4&embed_domain=the.streameast.app&dark_theme=1"]',
 
+    '.message-cont'
 ]
 //ids
 const chatBoxIDs = [
-    'chat-col',
+    //'chat-col',
     'chat-messages',
-    'iframeHolder',
     'chat-container'
     
 ]
